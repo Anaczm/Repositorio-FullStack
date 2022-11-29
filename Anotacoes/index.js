229b3aa0836bda2b277a2c1f4992f2a381f8ebed
Ana Carolina Zuliani Marinho <a.carolina.zumarinho@gmail.com>
2022-11-28 23:25:52 -0300

    anotações durante a aula

---
+++ Anotacoes/index.js
@@ -0,0 +1,18 @@
+/* Operador ternário
+
+const pontos = 109;
+
+const nivelUsuario = pontos >= 100 ? 'Usuário vip' : 'Usuário normal'
+
+console.log(nivelUsuario)*/
+
+// laço for
+
+const lista = ["mouse", "teclado", "lápis", "notebook"]
+
+for(let i = 0; i < lista.length; i++){
+    console.log(lista[i])
+}
+
+// exercício
+
