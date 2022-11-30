c0eba0f76c36912e77a3cf2c59805fcb95940076
Ana Carolina Zuliani Marinho <a.carolina.zumarinho@gmail.com>
2022-11-30 20:15:06 -0300

    anotações - aulas 52 e 53 - laço for

--- Anotacoes/index.js
+++ Anotacoes/index.js
@@ -1,11 +1,3 @@
-/* Operador ternário
-
-const pontos = 109;
-
-const nivelUsuario = pontos >= 100 ? 'Usuário vip' : 'Usuário normal'
-
-console.log(nivelUsuario)*/
-
 // laço for
 
 const lista = ["mouse", "teclado", "lápis", "notebook"]
@@ -14,5 +6,25 @@ for(let i = 0; i < lista.length; i++){
     console.log(lista[i])
 }
 
-// exercício
+    // exercício/aula 53
+    const elementos = [
+        {tag: 'h1', texto: 'tapioca homofóbica'},
+        {tag: 'h2', texto: 'tapioca homofóbica2'},
+        {tag: 'h3', texto: 'tapioca homofóbica3'}
+    ]
 
+    const content = document.querySelector('content')
+    const h1 = document.createElement('h1')
+
+    for(i = 0; i < elementos.length; i++){
+        let {tag, texto} = elementos[i]
+        let tagCriada = document.createElement(tag)
+        let textoCriado = document.createTextNode(texto)
+
+        textoCriado.appendChild(textoCriado)
+        h1.appendChild(tagCriada)
+    }
+
+    content.appendChild(h1)
+
+    // DOM: documents objetive model
